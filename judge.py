4382da005fad6e6b71d29b6a6041ce881f882927
D4rk-N355 <dcdreamchaser01@gmail.com>
2025-11-23 03:28:48 +0800

    isec

--- judge.py
+++ judge.py
@@ -139,4 +139,4 @@ if __name__ == "__main__":
     os.makedirs("problems", exist_ok=True)
     os.makedirs("submissions", exist_ok=True)
     init_db()
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=3000)
